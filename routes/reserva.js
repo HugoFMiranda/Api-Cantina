555496e4109f900ad25afd15eca97858d0125cb3
Hugo Miranda <hugoeu10@gmail.com>
2022-11-14 11:56:41 +0000

    added get all from specific aluno + remove from specific aluno

--- routes/reserva.js
+++ routes/reserva.js
@@ -113,38 +113,40 @@ router
     }
 });
 
-
-// GET reservas by id with data of aluno retrieved from external api using node-rest-client
+// Get all reservas from a specific aluno with data of aluno retrieved from external api using node-rest-client
 router
-    .get('/:id', function(req, res, next) {
+    .get('/:num_aluno', function(req, res, next) {
         try {
             client.registerMethod("jsonMethod", url_API + '/api/alunos', "GET");
             client.methods.jsonMethod(function(data, response) {
-                Reserva.findById(req.params.id, function(err, reserva) {
-                    var reservaCompleta = {};
-                    if (reserva == null) {
-                        return res.json({ message: 'No reserva found' });
+                Reserva.find(function(err, reservas) {
+                    if (reservas.length == 0) {
+                        return res.json({ message: 'No reservas found' });
                     } else {
-                        if (reserva.aluno != null && reserva.aluno.num_aluno != null) {
-                            for (var j = 0; j < data.length; j++) {
-                                if (reserva.aluno.num_aluno == data[j].id) {
-                                    reservaCompleta = {
-                                        id: reserva.id,
-                                        aluno: {
-                                            num_aluno: data[j].id,
-                                            nome_aluno: data[j].nome,
-                                            email_aluno: data[j].email
-                                        },
-                                        data: reserva.data,
-                                        pratoReservado: reserva.pratoReservado,
-                                    };
+                        var reservas_alunos = [];
+                        for (var i = 0; i < reservas.length; i++) {
+                            if (reservas[i].aluno != null && reservas[i].aluno.num_aluno != null) {
+                                for (var j = 0; j < data.length; j++) {
+                                    if (reservas[i].aluno.num_aluno == data[j].id && reservas[i].aluno.num_aluno == req.params.num_aluno) {
+                                        reservas_alunos.push({
+                                            id: reservas[i].id,
+                                            aluno: {
+                                                num_aluno: data[j].id,
+                                                nome_aluno: data[j].nome,
+                                                email_aluno: data[j].email
+                                            },
+                                            data: reservas[i].data,
+                                            pratoReservado: reservas[i].pratoReservado,
+                                        });
+                                    }
                                 }
                             }
                         }
-                        return res.json({
-                            message: 'Reserva found',
-                            reserva: reservaCompleta
-                        });
+                        if (reservas_alunos.length == 0) {
+                            return res.json({ message: 'No reservas found with alunos' });
+                        } else {
+                            return res.json({ message: 'Reservas with alunos retrieved successfully', reservas: reservas_alunos });
+                        }
                     }
                 });
             });
@@ -153,26 +155,63 @@ router
         }
     })
 
-// DELETE a reserva by id
-.delete('/:id', function(req, res, next) {
-    Reserva.findByIdAndRemove(req.params.id, function(err, reserva) {
-        if (err) return next({ message: 'No reserva found', error: err });
-        return res.json({ message: 'Reserva deleted successfully' });
-    });
+
+// DELETE all reservas from a specific aluno with data of aluno retrieved from external api using node-rest-client
+.delete('/:num_aluno', function(req, res, next) {
+    try {
+        client.registerMethod("jsonMethod", url_API + '/api/alunos', "GET");
+        client.methods.jsonMethod(function(data, response) {
+            Reserva.find(function(err, reservas) {
+                if (reservas.length == 0) {
+                    return res.json({ message: 'No reservas found' });
+                } else {
+                    var reservas_alunos = [];
+                    for (var i = 0; i < reservas.length; i++) {
+                        if (reservas[i].aluno != null && reservas[i].aluno.num_aluno != null) {
+                            for (var j = 0; j < data.length; j++) {
+                                if (reservas[i].aluno.num_aluno == data[j].id && reservas[i].aluno.num_aluno == req.params.num_aluno) {
+                                    reservas_alunos.push({
+                                        id: reservas[i].id,
+                                        aluno: {
+                                            num_aluno: data[j].id,
+                                            nome_aluno: data[j].nome,
+                                            email_aluno: data[j].email
+                                        },
+                                        data: reservas[i].data,
+                                        pratoReservado: reservas[i].pratoReservado,
+                                    });
+                                }
+                            }
+                        }
+                    }
+                    if (reservas_alunos.length == 0) {
+                        return res.json({ message: 'No reservas found with alunos' });
+                    } else {
+                        for (var k = 0; k < reservas_alunos.length; k++) {
+                            try {
+                                Reserva.remove({
+                                    _id: reservas_alunos[k].id
+                                });
+                            } catch (error) {
+                                return res.json({ message: 'Error', error: err });
+                            }
+                        }
+                        return res.json({ message: 'Reservas with alunos deleted successfully', reservas: reservas_alunos });
+                    }
+                }
+            });
+        });
+    } catch (err) {
+        return res.json({ message: 'Error', error: err });
+    }
 })
 
 // PUT a reserva by id
 .put('/:id', function(req, res, next) {
-    Reserva.findByIdAndUpdate(req.params.id, req.body, function(err, reserva) {
+    rva.findByIdAndUpdate(req.params.id, req.body, function(err, reserva) {
         if (err) return next({ message: 'No reserva found', error: err });
         return res.json({ message: 'Reserva updated successfully' });
     });
 });
-
-
-
-
-
-
 
 module.exports = router;
